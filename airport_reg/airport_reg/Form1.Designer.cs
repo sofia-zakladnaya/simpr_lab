5471782d880514529ec94c46657f709403679324
Sofia <sofia.zakladnaya@gmail.com>
2017-11-18 22:37:00 +0300

    Source image created

---
+++ airport_reg/airport_reg/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace airport_reg
+{
+    partial class RegForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbCurPicture = new System.Windows.Forms.PictureBox();
+            this.lbFlightSchedule = new System.Windows.Forms.ListBox();
+            this.tbTicketInfo = new System.Windows.Forms.TextBox();
+            this.lbScheduleTitle = new System.Windows.Forms.Label();
+            this.lbTicketInfoTitle = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCurPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbCurPicture
+            // 
+            this.pbCurPicture.Location = new System.Drawing.Point(12, 7);
+            this.pbCurPicture.Name = "pbCurPicture";
+            this.pbCurPicture.Size = new System.Drawing.Size(895, 515);
+            this.pbCurPicture.TabIndex = 0;
+            this.pbCurPicture.TabStop = false;
+            // 
+            // lbFlightSchedule
+            // 
+            this.lbFlightSchedule.FormattingEnabled = true;
+            this.lbFlightSchedule.Location = new System.Drawing.Point(913, 33);
+            this.lbFlightSchedule.Name = "lbFlightSchedule";
+            this.lbFlightSchedule.Size = new System.Drawing.Size(217, 225);
+            this.lbFlightSchedule.TabIndex = 1;
+            // 
+            // tbTicketInfo
+            // 
+            this.tbTicketInfo.Location = new System.Drawing.Point(913, 376);
+            this.tbTicketInfo.Multiline = true;
+            this.tbTicketInfo.Name = "tbTicketInfo";
+            this.tbTicketInfo.Size = new System.Drawing.Size(217, 146);
+            this.tbTicketInfo.TabIndex = 2;
+            // 
+            // lbScheduleTitle
+            // 
+            this.lbScheduleTitle.AutoSize = true;
+            this.lbScheduleTitle.Location = new System.Drawing.Point(910, 9);
+            this.lbScheduleTitle.Name = "lbScheduleTitle";
+            this.lbScheduleTitle.Size = new System.Drawing.Size(43, 13);
+            this.lbScheduleTitle.TabIndex = 3;
+            this.lbScheduleTitle.Text = "Рейсы:";
+            // 
+            // lbTicketInfoTitle
+            // 
+            this.lbTicketInfoTitle.AutoSize = true;
+            this.lbTicketInfoTitle.Location = new System.Drawing.Point(910, 360);
+            this.lbTicketInfoTitle.Name = "lbTicketInfoTitle";
+            this.lbTicketInfoTitle.Size = new System.Drawing.Size(120, 13);
+            this.lbTicketInfoTitle.TabIndex = 4;
+            this.lbTicketInfoTitle.Text = "Информация о билете";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 535);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1118, 103);
+            this.textBox1.TabIndex = 5;
+            // 
+            // RegForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1142, 646);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbTicketInfoTitle);
+            this.Controls.Add(this.lbScheduleTitle);
+            this.Controls.Add(this.tbTicketInfo);
+            this.Controls.Add(this.lbFlightSchedule);
+            this.Controls.Add(this.pbCurPicture);
+            this.Name = "RegForm";
+            this.Text = "Регистрация";
+            ((System.ComponentModel.ISupportInitialize)(this.pbCurPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbCurPicture;
+        private System.Windows.Forms.ListBox lbFlightSchedule;
+        private System.Windows.Forms.TextBox tbTicketInfo;
+        private System.Windows.Forms.Label lbScheduleTitle;
+        private System.Windows.Forms.Label lbTicketInfoTitle;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ airport_reg/airport_reg/simpr.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace WindowsApplication4 // не забудьте поменять на свой namespace //
+{
+
+    public class MyHookClass : NativeWindow
+    {
+        //
+        //   Класс работы с сообщениями.
+        //   Автор: Кузнецов Андрей (А-16-05)
+        //
+        //   Примечание: для выполнение действий в этом классе рекомендуется создать 
+        //   элемент класса, который используется для работы основной программы 
+        //   например: public solver solv; // в объявлениях
+        //   тогда в WndProc доступ к функциям будет как solv.function(...)
+        //   а доступ из основной программы(с формы) к значеним и функциям решателя
+        //   будет осуществлён так:
+        //   MyHookClass hook; // в пространстве имён формы
+        //   hook= new MyHookClass(); // в инициализаторе формы
+        //   hook.solv.function(...); // доступ к значениям и функциям (например из таймера, для обновления графики)
+        //   
+        //  
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern uint RegisterWindowMessage(string lpString);
+        
+        
+        //public solver solv; // - пример включения класса "решателя"
+        
+        
+        uint simpr;
+
+        public MyHookClass(IntPtr hWnd)
+        {
+            simpr = RegisterWindowMessage("MyMessage"); // регистрируем своё сообщение
+            this.AssignHandle(hWnd);
+            // solv = new solver(); // пример создания элемента класса "решателя"
+
+        }
+
+        protected override void WndProc(ref Message m) // в эту функцию приходят все сообщения от СИМПРА
+        {
+
+            int i = 0,wparamhi,wparamlo,wparam;
+            int lParam = Convert.ToInt32("" + m.LParam);
+
+            if (m.Msg == simpr)
+            {
+
+                wparam = Convert.ToInt32("" + m.WParam);
+                wparamhi = wparam / 65536;
+                wparamlo = wparam - wparamhi * 65536;
+
+                if (wparamhi == 0)//условия
+                {
+                    if (wparamlo == 1)// таблица 1 
+                    {
+                        if (lParam == 1)// таблица 1 условие 1
+                        {
+                            m.Result = new IntPtr(1); // вернуть условие 1 = TRUE
+                        }
+                        else if (lParam == 2)// таблица 1 условие 2
+                        {
+                            m.Result = new IntPtr(0); // вернуть условие 2 = FALSE
+                        }
+                    }
+                    else if (wparamlo == 2)// таблица 2 
+                    {
+                        if (lParam == 1)// таблица 2 условие 1
+                        {
+                        }
+                        else if (lParam == 2)// таблица 2 условие 2
+                        {
+                        }
+                    }
+                }
+                else if (wparamhi == 1)//действия
+                {
+                    if (wparamlo == 1)// таблица 1 
+                    {
+                        if (lParam == 1)// таблица 1 действие 1
+                        {
+                            // действие
+                        }
+                        else if (lParam == 2)// таблица 1 действие 2
+                        {
+                            // действие
+                        }
+                    }
+                    else if (wparamlo == 2)// таблица 2 
+                    {
+                        if (lParam == 1)// таблица 2 действие 1
+                        {
+                            // действие
+                        }
+                        else if (lParam == 2)// таблица 2 действие 2
+                        {
+                            // действие
+                        }
+                    }
+
+                Application.DoEvents();
+               // Thread.Sleep(300); // если у нас есть визуальное отображение, то задержку можно установить здесь                    
+                m.Result = new IntPtr(1); // ответом на запрос действия со стороны СИМПР должна быть единица
+                }
+            }
+            else
+            {
+                base.WndProc(ref m); // для всех действий не связанных с СИМПР возвращаем управление программе
+            }
+
+
+
+        }
+
+    }
+}
