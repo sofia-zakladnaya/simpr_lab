fdd57e3992cc266bfaf36843a41e0a23c101adea
Sofia <sofia.zakladnaya@gmail.com>
2017-11-19 14:54:50 +0300

    Flight and Schedule classes basis created

---
+++ airport_reg/airport_reg/Flight.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace airport_reg
+{
+    class Flight
+    {
+        public int Number; //Номер рейса
+        private FlightType Type; //Тип рейса
+        private FlightStatus Status; //Статус рейса
+
+    }
+
+    public enum FlightType
+    {
+        International,
+        Domestic
+    };
+
+    public enum FlightStatus
+    {
+        NoRegistration,
+        RegistrationOpen,
+        RegistrationClose,
+        Departed
+    };
+}
---
+++ airport_reg/airport_reg/Schedule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace airport_reg
+{
+    class Schedule
+    {
+        public Flight FlightList; //Список рейсов
+        private int Departures; //Количество отправленных рейсов
+
+
+        //Регистрация всех рейсов завершена?
+        public bool IsFinished()
+        {
+            //TODO
+            return true;
+        }
+
+        //Открыть регистрацию на рейс
+        public void Open(int FlightNumber)
+        {
+            //TODO
+        }
+
+        //Закрыть регистрацию на рейс
+        public void Close(int FlightNumber)
+        {
+            //TODO
+        }
+    }
+}
