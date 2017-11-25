432f6c35066f9f4b850e6a4f10d076a908695cf6
Sofia <sofia.zakladnaya@gmail.com>
2017-11-25 19:23:41 +0300

    -

--- airport_reg/airport_reg/Airport.cs
+++ airport_reg/airport_reg/Airport.cs
@@ -1,7 +1,24 @@
-﻿
+﻿using System.Drawing;
+using System.Windows.Forms;
 namespace airport_reg
 {
+    
     public class Airport
     {
+        const int FlightNum = 10;
+        const int TimeOut = 5000;
+
+        //public RegForm win;
+        public Schedule schedule;
+        public Image img;
+        
+        public Airport()
+        {
+            //Создаём расписание
+            schedule = new Schedule(FlightNum);
+            //Задаём стартовое изображение
+            img = new Bitmap(Application.StartupPath + "\\img\\start.png");
+            
+        }
     }
 }
